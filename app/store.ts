0dbb5e6c0493a552bf445283955e9de87096fa49
TinPuffer <nguyenchitinhhtdtr2018@gmail.com>
2023-05-26 13:19:49 +0700

    initial project

---
+++ app/store.ts
@@ -0,0 +1,29 @@
+import { Action, configureStore, Store, ThunkAction } from "@reduxjs/toolkit";
+import creatSAGA from "redux-saga";
+import rootSaga from "./rootSaga";
+import { createWrapper } from "next-redux-wrapper";
+import { authReducer } from "./splice/authSlipe";
+
+const sagaMiddleware = creatSAGA();
+export const store =
+  configureStore({
+    reducer: {
+      auth: authReducer,
+      // authMC: authMCReducer,
+      // comment: commentReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: false,
+      }).concat(sagaMiddleware),
+  });
+
+sagaMiddleware.run(rootSaga);
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
